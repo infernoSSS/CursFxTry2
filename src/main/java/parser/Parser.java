46197e8a19f5e5bee77fafdf871c6b834c4b3050
Tima <timaoleinik@gmail.com>
2021-05-27 00:09:46 +0300

    pseudo code for Parser.getData

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -13,6 +13,10 @@ public class Parser {
         boolean connected = arduino.openConnection();
         System.out.println("Соединение установлено: " + connected);
 
+        arduino.serialWrite("Some request");
+        String ans = arduino.serialRead(1);
+        if (ans != "1") return null;
+
         String data = arduino.serialRead();
 
         String[] result = data.split("\n");
