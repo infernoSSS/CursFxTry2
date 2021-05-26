b7fb98184f0916109fd09ff609a1cb550c20c359
Tima <timaoleinik@gmail.com>
2021-05-27 00:01:17 +0300

    pseudo code for Parser.getData

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -5,6 +5,25 @@ import java.util.ArrayList;
 public class Parser {
 
     public ArrayList<String> getData(){
+        /*
+
+        Scanner scanner = new Scanner(System.in);
+        Arduino arduino = new Arduino("COM52", 9600);
+
+        boolean connected = arduino.openConnection();
+        System.out.println("Соединение установлено: " + connected);
+
+        String data = arduino.serialRead();
+
+        String[] result = data.split("\n");
+
+        // String[] to ArrayList<String>
+
+        arduino.closeConnection();
+
+        }
+
+         */
         return null;
     }
 }
