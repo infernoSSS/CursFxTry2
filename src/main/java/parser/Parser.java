4dae55a1d02d1c96a3b31918de78930c2fff5e8b
Tima <timaoleinik@gmail.com>
2021-05-28 14:05:53 +0300

    JOPA

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,19 +1,24 @@
 package parser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
-    private String logs = "logs";
     private static Parser parser;
-    private MyArduino arduino = null;
+    private MyArduino arduino;
 
-    private Parser(){
-
+    public Parser(){
+        arduino = new MyArduino();
     }
 
     public boolean setCOMPort(String comPort){
         try {
+            // "/dev/ttyACM0"
             arduino.setPortDescription(comPort);
             arduino.setBaudRate(9600);
-        }catch(Exception e){
+            arduino.openConnection();
+        } catch(Exception e){
             return false;
         }
         return true;
@@ -26,19 +31,48 @@ public class Parser {
         return parser;
     }
 
-    public String getData(){
-        // COM52
+    public String getData() {
         // /dev/ttyACM0
-        //arduino = new MyArduino("/dev/ttyACM0", 9600);
-        StringBuilder bld = new StringBuilder();
+        //System.out.println("Соединение установлено: " + arduino.openConnection());
+        if (!arduino.openConnection()) return null;
 
-        System.out.println("Соединение установлено: " + arduino.openConnection());
+        arduino.serialWrite('1');
+        String result = arduino.serialRead(0);
+        StringBuffer buffer = new StringBuffer();
 
-        arduino.serialWrite('5');
-        bld.append(arduino.serialRead(0));
+        //System.out.println(Arrays.toString(result.replace(".", " ").split(" ")));
+        List<String> list = Arrays.stream(result.replace(".", " ").split(" ")).filter(x -> !x.equals("end\n")).map(s -> {
+            buffer.append("Card ");
+            buffer.append(s.substring(0, 7));
+            buffer.append(" was ");
+            switch(Integer.parseInt(s.substring(8,9))){
+                case 0:
+                    buffer.append("Access granted");
+                    break;
+                case 1:
+                    buffer.append("Access denied");
+                    break;
+                case 2:
+                    buffer.append("Add card");
+                    break;
+                case 3:
+                    buffer.append("Delete card");
+                    break;
+                case 4:
+                    buffer.append("Entry program mode");
+                    break;
+                case 5:
+                    buffer.append("Exiting program mode");
+                    break;
+                case 6:
+                    buffer.append("Add mater card");
+                    break;
+            }
+            return buffer.append("\n").toString();
+        }).collect(Collectors.toList());
 
         arduino.closeConnection();
 
-        return bld.toString();
+        return buffer.toString();
     }
 }
